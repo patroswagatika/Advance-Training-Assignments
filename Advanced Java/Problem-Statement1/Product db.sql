545f67e84e95c34c05aafe2b7f5f8ee3c449ea22
swagatika patro <swagatikapatro7@gmail.com>
2022-05-03 17:15:27 +0530

    files added

--- Advanced Java/Problem-Statement1/Product db.sql
+++
@@ -1,5 +0,0 @@
-Create database Productdb ;
-use Productdb ;
-create table Product (ProductID varchar(5) Primary key  ,ProductName Varchar(30) , Product_Price varchar(10) );
-select * from  Product ;
- 
